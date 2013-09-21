d1ee357422c7d632417469b83cf5ac6c34338249
John Paul <john@johnkpaul.com>
2013-09-20 23:38:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# innersource
+
+module to get the source of a function as a string
+
+## Usage
+
+```javascript
+function test1() {
+  console.log('hello');
+}
+
+console.log(innersource(test1)); //  console.log('hello');
+```
---
+++ test/test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var fs = require('fs');
+var assert = require('assert');
+var innersource = require('../index');
+
+function test1() {
+  console.log('hello');
+}
+
+assert.equal(innersource(test1).trim(), "console.log('hello');");
+
+var test2 = function () {
+  console.log('hello');
+};
+
+assert.equal(innersource(test2).trim(), "console.log('hello');");
+
+var test3 = function test() {
+  console.log('hello');
+};
+
+assert.equal(innersource(test3).trim(), "console.log('hello');");
+
+function   test4() {
+  console.log('hello');
+}
+
+assert.equal(innersource(test4).trim(), "console.log('hello');");
